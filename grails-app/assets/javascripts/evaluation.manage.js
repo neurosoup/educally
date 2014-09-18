cbc8e9784f2e6da78212d6a73e7275c7e6d2c018
neurosoup <neurosoup@gmail.com>
2014-09-18 07:39:52 +0200

    evaluation screen

--- grails-app/assets/javascripts/evaluation.manage.js
+++ grails-app/assets/javascripts/evaluation.manage.js
@@ -14,48 +14,48 @@ var buildSkillTree = function (data, root) {
     var rootElement = $(root);
     var skills = JSON.parse(data);
 
-    var html = "<ol class='dd-list'><li class='dd-item'><div class='dd-handle'></div></li></ol>";
-    var itemTemplate = $("<div/>").html(html);
+    var itemHtml = "<li class='dd-item'><div class='dd-handle'></div></li>";
+    var listHtml = "<ol class='dd-list'></ol>";
+    var itemTemplate = $("<div/>").html(itemHtml);
+    var listTemplate = $("<div/>").html(rootHtml);
 
     //console.log(skills);
 
     for (var i = 0, len = skills.length; i < len; i++) {
 
         var itemElement = itemTemplate.clone();
+        var listElement = listTemplate.clone();
+        var itemDataElement = itemElement.find("li");
+        var listDataElement = itemElement.find("ol");
 
         var skill = skills[i];
 
-        var dataElement = itemElement.find("ol");
-
-        dataElement.attr("data-id", skill.id);
+        itemDataElement.attr("data-id", skill.id);
         itemElement.find(".dd-handle").html(skill.title);
-
         if (skill.name) {
-            dataElement.attr("data-name", skill.name)
+            itemDataElement.attr("data-name", skill.name);
+            listDataElement.attr("data-name", skill.name);
         }
 
+        //Determine parent name
+        var parentName = "";
+        var parent = null;
         if (skill.path) {
             var path = skill.path.split(",");
-            var parentName = path[path.length - 2];
-            var parentCandidate = rootElement.find("[data-name='" + parentName + "']");
-            var parent = parentCandidate;
-
-            if (parentCandidate.is("ol")) {
-                parent = parentCandidate.children().first();
-                parentCandidate.attr("data-name", "");
-                parent.attr("data-name", parentName);
-            }
-            parent.append(itemElement.html());
-
-            //console.log("name=" + skill.name + " parent=" + parentName + "\n parent=" + parent+ " html=" + parent.html());
-
-
+            parentName = path[path.length - 2];
         } else {
-            var newRoot = itemElement.children().first();
-            newRoot.find("li").attr("data-name", skill.name)
-            rootElement.append(newRoot.html());
-            console.log(newRoot.html());
+            parentName = "root-list";
         }
+
+        //Find parent and insert item element
+        parent = rootElement.find("li[data-name='" + parentName + "']");
+        if (!parent) {
+            parent = rootElement.append(listElement.html());
+        }
+        parent.append(itemElement.html());
+
+
+        //console.log("name=" + skill.name + " parent=" + parentName + "\n parent=" + parent+ " html=" + parent.html());
     }
 
     $('#nestable').nestable();
@@ -75,10 +75,10 @@ var pagefunction = function () {
     };
 
     // activate Nestable for list 1
-  /*  $('#nestable').nestable().on('change', updateOutput);
+    /*  $('#nestable').nestable().on('change', updateOutput);
 
-    // output initial serialised data
-    updateOutput($('#nestable').data('output', $('#nestable-output')));*/
+     // output initial serialised data
+     updateOutput($('#nestable').data('output', $('#nestable-output')));*/
 
     $('#nestable-menu').on('click', function (e) {
         var target = $(e.target), action = target.data('action');
