e793fb6f2d57152ba90e7a08481e484d733338df
Albert Zémour <lawrence-albert.zemour@cgm.com>
2014-09-30 17:44:12 +0200

    evaluations screen

--- grails-app/controllers/educally/EvaluationController.groovy
+++ grails-app/controllers/educally/EvaluationController.groovy
@@ -22,12 +22,9 @@ class EvaluationController {
         def skillCoverage = evaluationService.calculateSkillCoverage(teacher, skillBook)
         def skills = skillBook.skills.sort { it.path }
 
-        def evaluatedSkills = evaluations.values.flatten().groupBy { it.skillId }
-
-
+        def evaluatedSkills = evaluations.values.flatten().groupBy { it.skill }
 
         [skills: skills as JSON, evaluatedSkills: evaluatedSkills, skillBookId: skillBook.id, skillBookTitle: skillBook.title, evaluationCount: evaluations.size(), skillCoverage: skillCoverage]
-
     }
 
 }
