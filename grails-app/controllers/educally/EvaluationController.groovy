e13595de751151ddd2d394f13f4730014ce5e5c0
Albert Zémour <lawrence-albert.zemour@cgm.com>
2014-10-03 18:08:03 +0200

    evaluations screen

--- grails-app/controllers/educally/EvaluationController.groovy
+++ grails-app/controllers/educally/EvaluationController.groovy
@@ -20,13 +20,15 @@ class EvaluationController {
 
         def evaluations = teacher.pupils.flatten().evaluations.flatten().groupBy { x -> x.values.sort { y -> y.skillId }.skillId }
         def skills = skillBook.skills.sort { it.path }
-        BigDecimal skillCount = skills.findAll { !it.name } as BigDecimal
+        BigDecimal skillCount = skills.findAll { !it.name }.size()
         BigDecimal evaluatedCount = evaluations.values().flatten().size()
         def skillCoverage = skillCount > 0 ? Math.round(evaluatedCount / skillCount * 100) : 0
 
         def evaluatedSkills = []
         evaluations.each { k, v ->
-            evaluatedSkills.add([skill: skills.find { y -> y.id == x.key }, evaluations: x.value])
+            k.each {x ->
+                evaluatedSkills.add([skill: skills.find { y -> y.id == x }, evaluations: v])
+            }
         }
 
         [skills: skills as JSON, evaluatedSkills: evaluatedSkills, skillBookId: skillBook.id, skillBookTitle: skillBook.title, evaluationCount: evaluations.size(), skillCoverage: skillCoverage]
